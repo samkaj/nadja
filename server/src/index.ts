eed08fa0dfbacb7f8dec69bfc23720607ed3c679
samkaj <kajavasamuel@gmail.com>
2023-03-07 21:22:16 +0100

    Initialize server
    This is the beginning of the server part of nadja.

---
+++ server/src/index.ts
@@ -0,0 +1,11 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server started on http://localhost:${port}`);
+});
