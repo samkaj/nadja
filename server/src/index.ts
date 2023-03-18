704e3bc29ebfa300f5fe2662a35b4767f244f4fb
samkaj <kajavasamuel@gmail.com>
2023-03-19 00:49:42 +0100

    Add routes for Spotify auth

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,8 +2,17 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 import express from "express";
-
 const app = express();
+
+import { callbackRouter } from "./routes/callback";
+import { loginRouter } from "./routes/login";
+import { refreshRouter } from "./routes/refresh";
+
+export const router = express.Router();
+app.use(callbackRouter);
+app.use(loginRouter);
+app.use(refreshRouter);
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
