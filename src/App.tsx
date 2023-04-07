ffbe97cf6005be88bf02c33a64d25f9a7116e5fb
samkaj <kajavasamuel@gmail.com>
2023-04-07 20:48:25 +0200

    Add dependency for icons

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,19 @@ import Home from "./pages/Home";
 import { UserContextProvider } from "./context/User";
 import Callback from "./pages/Callback";
 import { Routes, Route } from "react-router-dom";
+import "./css/main.css";
+import { IconContext } from "react-icons";
 
 const App = () => {
   return (
-    <UserContextProvider>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/callback" element={<Callback />} />
-      </Routes>
-    </UserContextProvider>
+    <IconContext.Provider value={{ className: "react-icons" }}>
+      <UserContextProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/callback" element={<Callback />} />
+        </Routes>
+      </UserContextProvider>
+    </IconContext.Provider>
   );
 };
 
