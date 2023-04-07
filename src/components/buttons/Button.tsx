30f8e90fd0ca4b19bd0643e6209721ad7618ff9a
samkaj <kajavasamuel@gmail.com>
2023-04-07 20:51:50 +0200

    Add generic button component

---
+++ src/components/buttons/Button.tsx
@@ -0,0 +1,49 @@
+import { IconType } from "react-icons";
+import "./button.css";
+
+type ButtonProps = {
+  Icon?: IconType;
+  onClick?: () => void;
+  className?: string;
+};
+
+const Button = ({
+  Icon,
+  onClick,
+  className,
+  children,
+}: React.PropsWithChildren<ButtonProps>): JSX.Element => (
+  <button className={className} onClick={onClick}>
+    {Icon && <Icon className="white-fill" />}
+    {children}
+  </button>
+);
+
+export const PrimaryButton = ({
+  onClick,
+  children,
+}: React.PropsWithChildren<ButtonProps>): JSX.Element => (
+  <button onClick={onClick} className="primary">
+    {children}
+  </button>
+);
+
+export const SecondaryButton = ({
+  onClick,
+  children,
+}: React.PropsWithChildren<ButtonProps>): JSX.Element => (
+  <button onClick={onClick} className="secondary">
+    {children}
+  </button>
+);
+
+export const TertiaryButton = ({
+  onClick,
+  children,
+}: React.PropsWithChildren<ButtonProps>): JSX.Element => (
+  <button onClick={onClick} className="tertiary">
+    {children}
+  </button>
+);
+
+export default Button;
