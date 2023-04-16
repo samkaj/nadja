2d56512bb46caa62743e0a1795d284ff421e224e
samkaj <kajavasamuel@gmail.com>
2023-04-16 18:09:31 +0200

    Implement generator frontend

--- src/views/dashboard/Dashboard.tsx
+++ src/views/dashboard/Dashboard.tsx
@@ -1,15 +1,194 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { UserContext } from "../../context/User";
+import { search } from "../../playlist-generator/Search";
+import "./dashboard.scss";
+import "../../shared/shadow.scss";
+import { FaSearch } from "react-icons/fa";
+import { Track } from "../../playlist-generator/spotify-objects/Track";
+import { PrimaryButton } from "../../components/buttons/Button";
+import { getRecommendations } from "../../playlist-generator/PlaylistGenerator";
 
 const Dashboard = () => {
   const { user } = useContext(UserContext);
+  const [searchResults, setSearchResults] = useState<Track[]>([]);
+  const [selectedTracks, setSelectedTracks] = useState<Track[]>([]);
+  const [playlistName, setPlaylistName] = useState<string>("");
+  const [description, setDescription] = useState<string>("");
+  const [energy, setEnergy] = useState<number>(0.5);
+  const [danceability, setDanceability] = useState<number>(0.5);
+
+  const handlePlaylistNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPlaylistName(e.target.value);
+  };
+
+  const handleDescriptionChange = (
+    e: React.ChangeEvent<HTMLTextAreaElement>
+  ) => {
+    setDescription(e.target.value);
+  };
+
+  const handleEnergyChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEnergy(parseFloat(e.target.value) / 100);
+  };
+
+  const handleDanceabilityChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setDanceability(parseFloat(e.target.value) / 100);
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const form = e.target as HTMLFormElement;
+    const input = form.elements[0] as HTMLInputElement;
+    const query = input.value;
+    const results = await search(query, user?.accessToken ?? "");
+    setSearchResults(results);
+    console.log(results);
+  };
+
+  const handlePlaylistSubmit = async () => {
+    const playlist = await getRecommendations({
+      playlistName: playlistName,
+      description,
+      energy,
+      danceability,
+      selectedTracks,
+      accessToken: user?.accessToken ?? "",
+    });
+    console.log(playlist);
+  };
+
+  type SearchResultProps = {
+    track: Track;
+  };
+
+  const TrackContainer = ({ track }: SearchResultProps) => {
+    const [isSelected, setIsSelected] = useState(false);
+
+    const onSelect = () => {
+      if (selectedTracks.length >= 5) {
+        return;
+      }
+
+      if (selectedTracks.some((t) => t.id === track.id)) {
+        setSelectedTracks((prev) => prev.filter((t) => t.id !== track.id));
+        setSearchResults((prev) => [...prev, track]);
+      } else {
+        setSelectedTracks((prev) => [...prev, track]);
+        setSearchResults((prev) => prev.filter((t) => t.id !== track.id));
+      }
+      setIsSelected((prev) => !prev);
+    };
+
+    return (
+      <li
+        onClick={onSelect}
+        key={track.id}
+        className={`track ${isSelected ? "selected" : ""}`}
+      >
+        <img src={track.albumArtUrl} alt="" />
+        <div className="track-info">
+          <span className="track-name">{track.name}</span>
+          <br />
+          <span className="track-artist">
+            {track.artists.map((artist) => artist).join(", ")}
+          </span>
+        </div>
+      </li>
+    );
+  };
 
   return (
     <main>
-      <h1>Hi mom</h1>
-      <p>Logged in as {user?.name}</p>
+      <div className="full-width generator-wrapper">
+        <h1 className="large">Playlist Generator</h1>
+        <p>
+          Search -{">"} Select 5 tracks -{">"} Select mood -{">"} Choose name -
+          {">"} Generate.
+        </p>
+        <form onSubmit={handleSubmit} className="search shadow ">
+          <FaSearch />
+          <input className="medium" placeholder="Search..." type="text" />
+        </form>
+      </div>
+      <section className="results shadow">
+        <div className="left">
+          <h1 className="shadow">Search Results</h1>
+          <ul>
+            {searchResults.map((track) => {
+              return <TrackContainer track={track} />;
+            })}
+          </ul>
+        </div>
+
+        <aside className="right">
+          <h1>Details</h1>
+          <label htmlFor="name">Name</label>
+          <input
+            onInput={handlePlaylistNameChange}
+            type="text"
+            name="Name"
+            id="name"
+            maxLength={100}
+            placeholder="Playlist name..."
+          />
+          <label htmlFor="description">Description</label>
+          <textarea
+            onInput={handleDescriptionChange}
+            name="Description"
+            id="description"
+            rows={4}
+            maxLength={300}
+            placeholder="Optional description..."
+          />
+          <h1>Mood</h1>
+          <div className="mood-sliders">
+            <div className="slider">
+              <label htmlFor="energy">Energy</label>
+              <input
+                onInput={handleEnergyChange}
+                type="range"
+                name="Energy"
+                id="energy"
+                min={0}
+                max={100}
+              />
+            </div>
+            <div className="slider">
+              <label htmlFor="danceability">Danceability</label>
+              <input
+                onInput={handleDanceabilityChange}
+                type="range"
+                name="Danceability"
+                id="danceability"
+                min={0}
+                max={100}
+              />
+            </div>
+          </div>
+          <div className="selected-tracks">
+            <h1>Selected Tracks [{selectedTracks.length}/5]</h1>
+            <ul>
+              {selectedTracks.map((track) => {
+                return <TrackContainer track={track} />;
+              })}
+            </ul>
+          </div>
+          {selectedTracks.length === 5 && (
+            <PrimaryButton
+              onClick={handlePlaylistSubmit}
+              className="bottom medium"
+            >
+              Generate
+            </PrimaryButton>
+          )}
+          {selectedTracks.length < 5 && (
+            <PrimaryButton className="bottom medium disabled">
+              Generate
+            </PrimaryButton>
+          )}
+        </aside>
+      </section>
     </main>
   );
 };
-
 export default Dashboard;
