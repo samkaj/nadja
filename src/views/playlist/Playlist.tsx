9b7a5a06dbdbc573084fa0de9e099ea82a89466c
samkaj <kajavasamuel@gmail.com>
2023-10-17 12:25:29 +0200

    Feat: add playlist component, hover styling todo

--- src/views/playlist/Playlist.tsx
+++ src/views/playlist/Playlist.tsx
@@ -1,32 +1,14 @@
-// React component for showing a playlist
+import "../playlist-generator/generator.scss";
+import TrackContainer from "../playlist-generator/TrackContainer";
+import { SpotifyObjectFactory } from "../../playlist-generator/SpotifyObjectFactory";
 
-/*
- * Playlist contains a list of tracks.
- * A track is:
- * {
- *  id: string;
- *  name: string;
- *  artists: [{ name: string }];
- *  album: { name: string, images: [{ url: string }], external_urls: { spotify: string } };
- *  preview_url: string;
- *
- * }
- */
 const Playlist = (playlist: any) => {
   return (
     <div className="playlist">
-      <h1>Playlist</h1>
+      <h1>Playlist: {playlist.playlist.name}</h1>
       <div className="tracks">
         {playlist.playlist.tracks.map((track: any) => (
-          <div className="track">
-            <img src={track.album.images[0].url} alt="album cover" />
-            <div className="track-info">
-              <a href={track.album.external_urls.spotify}>
-                <h2>{track.name}</h2>
-              </a>
-              <h3>{track.artists[0].name}</h3>
-            </div>
-          </div>
+          <TrackContainer onClick={()=>{}} track={SpotifyObjectFactory.fromSpotifyTrack(track)} />
         ))}
       </div>
     </div>
